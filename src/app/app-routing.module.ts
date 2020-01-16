6970e637fc0a52fa97477b0afbb911eab0b2bcc3
Yazeed Hammad <Yazeed.H@CapitalBank.jo>
2020-01-16 14:34:49 +0200

    (add) nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", loadChildren: "./about/about.module#AboutModule" },
+  { path: "fight", loadChildren: "./fight-game/fight.module#FightModule" },
+  { path: "math", loadChildren: "./math-game/math.module#MathModule" },
+  { path: "race", loadChildren: "./race-game/race.module#RaceModule" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { FightGameComponent } from './fight-game/fight-game.component';
-import { HomeComponent } from './home/home.component';
-import { MathGameComponent } from './math-game/math-game.component';
-import { NavComponent } from './nav/nav.component';
-import { RaceGameComponent } from './race-game/race-game.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AlertModule } from "ngx-bootstrap";
+import { HttpClientModule, HttpClientJsonpModule } from "@angular/common/http";
+import { SharedModule } from "./shared/shared.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+// import { CountUpModule } from 'countup.js-angular2';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    FightGameComponent,
-    HomeComponent,
-    MathGameComponent,
-    NavComponent,
-    RaceGameComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    HttpClientJsonpModule,
+    BrowserAnimationsModule,
+    // CountUpModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
